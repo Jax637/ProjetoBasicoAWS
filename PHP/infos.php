141f3c326ef6f61de3f88201dfa6310cabbe0c99
Jax637 <86756006+Jax637@users.noreply.github.com>
2023-08-25 14:13:00 -0300

    Add files via upload

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "concessionaria.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
+$username = "jason";
+$password = "ehysuRkvhYqchNpoc2WG";
+$database = "db_conce";
+?>
